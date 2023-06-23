b04c40645e01a9233dbef011da225268333156b1
Vasiliy <135667979+Vasiliy-Vorobiov@users.noreply.github.com>
2023-06-23 13:42:05 +0300

    include lessons 1-4 and a litle 5

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,45 @@
+class QuestionsController < ApplicationController
+  # нужна для отключения токенов при работе через команду CURL
+  # для работы с формами в браузере токены нужны
+  # skip_before_action :verify_authenticity_token
+
+  def create
+    question = Question.create(
+      body: params[:question][:body],
+      user_id: params[:question][:user_id]
+    )
+
+    redirect_to question_path(question)
+    # redirect_to '/' # корневой адрес на сайте
+    # render text: 'ваш запрос обработан'
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    @question.update(
+      body: params[:question][:body],
+      user_id: params[:question][:user_id]
+    )
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def index
+    # в этом случае сортировка будет средставми RUBY , а не средствами SQL
+    # @questions = Question.all.sort_by(&:user_id)
+    # в этом случае сортировка будет средставми SQL 'user_id DESC' - По убыванию. По умолчанию ASC
+    @questions = Question.all.order('user_id')
+  end
+
+  def new
+    @question = Question.new
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # post '/questions', to: 'questions#create'
+  # patch '/questions/:id', to: 'questions#update'
+  # delete '/questions/:id', to: 'questions#destroy'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # заменяем всё на одну строчку, что то же самое
+  resources :questions
 end
