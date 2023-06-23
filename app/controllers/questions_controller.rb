848c54fd3d5f6d0a907e0f3fe38893c6aea0b2ca
Vasiliy <135667979+Vasiliy-Vorobiov@users.noreply.github.com>
2023-06-23 19:00:27 +0300

    refactoring  lesson 5

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,37 +1,46 @@
 class QuestionsController < ApplicationController
+  # Теперь поиск вопроса в БД можно удалить из этих действий
+  before_action :set_question, only: %i[edit update show destory]
+
   # нужна для отключения токенов при работе через команду CURL
   # для работы с формами в браузере токены нужны
   # skip_before_action :verify_authenticity_token
 
   def create
-    question = Question.create(
-      body: params[:question][:body],
-      user_id: params[:question][:user_id]
-    )
-
+    # question = Question.create(params[:questions]) - Так делать не безопасно,
+    # т.к. злоумышленник со страницы в браузере может передать любые значения параметров
+    # но рельсы имеют спец методы у объекта params, которые ограничиваю поля, которые можно передать через параметры в модель
+    #
+    # require - требует чтобы у хэша params был конкретный ключ
+    # permit - отсавляет в том объекте, у кторого он был вызван только заданные значения ключей
+    question = Question.create(question_params)
     redirect_to question_path(question)
     # redirect_to '/' # корневой адрес на сайте
     # render text: 'ваш запрос обработан'
   end
 
   def update
-    @question = Question.find(params[:id])
-    @question.update(
-      body: params[:question][:body],
-      user_id: params[:question][:user_id]
-    )
+    # @question = Question.find(params[:id])
+    
+    # @question.update(
+    #   body: params[:question][:body],
+    #   user_id: params[:question][:user_id]
+    # )
+    # Заменяем на:
+    @question.update(question_params)
+
     redirect_to question_path(@question)
   end
 
   def destroy
-    @question = Question.find(params[:id])
+    # @question = Question.find(params[:id])
     @question.destroy
 
     redirect_to questions_path
   end
 
   def show
-    @question = Question.find(params[:id])
+    # @question = Question.find(params[:id])
   end
 
   def index
@@ -46,6 +55,16 @@ class QuestionsController < ApplicationController
   end
 
   def edit
+    @question = Question.find(params[:id])
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:body, :user_id)
+  end
+
+  def set_question
     @question = Question.find(params[:id])
   end
 
